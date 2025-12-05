1683d1ae62852ccebf70d12a952eecfb2c98fa42
kganitis <ganitiskostas@gmail.com>
2025-12-05 19:11:30 +0200

    v0.1.2

--- src/bamengine/__init__.py
+++ src/bamengine/__init__.py
@@ -166,7 +166,7 @@ Notes
 
 from __future__ import annotations
 
-__version__: str = "0.1.1"
+__version__: str = "0.1.2"
 
 # ============================================================================
 # Standard library imports
