8024ce8afa72a34c0f7be97cd6d732c037c03f9c
kganitis <ganitiskostas@gmail.com>
2026-03-10 11:09:30 +0200

    chore: bump version to 0.6.1

--- src/bamengine/__init__.py
+++ src/bamengine/__init__.py
@@ -166,7 +166,7 @@ Notes
 
 from __future__ import annotations
 
-__version__: str = "0.6.0"
+__version__: str = "0.6.1"
 
 # ============================================================================
 # Standard library imports
